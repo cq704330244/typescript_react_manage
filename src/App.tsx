3ed862689effdbf727bbf7749748d1810ecf1393
LAPTOP-N7IVA5JC\chengqi <704330244@qq.com>
2021-04-10 09:18:32 +0800

    fix: add antd  && prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Button } from 'antd'
 import './style/index.less';
 
 function App() {
-  return <div className='App'>2211</div>;
+  return <div className='App'>
+    <Button>22222</Button>
+  </div>;
 }
 
 export default App;
